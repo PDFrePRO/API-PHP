f19c0424f956bebf441ababb14c1a620c7739262
bkr <bernhard.krop@richter-poweleit.de>
2025-11-20 17:07:26 +0100

    * The following backwards incompatible changes have been implemented:     * `\PDFrePRO\Exception\Exception` is no longer extending `__construct()` and `__toString()`.     * Removed all constants `\PDFrePRO\Exception\Exception::CODE_*`.

--- src/PDFrePRO/Exception/Exception.php
+++ src/PDFrePRO/Exception/Exception.php
@@ -23,7 +23,6 @@ namespace PDFrePRO\Exception;
 //****************************************************************************************************************************************\\
 
 use RuntimeException;
-use Throwable;
 
 //****************************************************************************************************************************************\\
 //                                                                                                                                        \\
@@ -38,52 +37,4 @@ use Throwable;
  * @description This throwable will be thrown by the PDFrePRO library on any error.
  * @link        https://www.pdfrepro.de/
  */
-class Exception extends RuntimeException
-{
-    //************************************************************************************************************************************\\
-    //                                                                                                                                    \\
-    //                                                        Error Code Constants                                                        \\
-    //                                                                                                                                    \\
-    //************************************************************************************************************************************\\
-
-    /**
-     * This code is set for {@see Exception}s by default.
-     */
-    public const CODE_DEFAULT                      = 0;
-
-    /**
-     * This code is set for {@see Exception}s, if the minimum version requirement for PHP is not met.
-     */
-    public const CODE_MINIMUM_REQUIRED_PHP_VERSION = 1;
-
-    //************************************************************************************************************************************\\
-    //                                                                                                                                    \\
-    //                                                            Magic Methods                                                           \\
-    //                                                                                                                                    \\
-    //************************************************************************************************************************************\\
-
-    /**
-     * The constructor of this throwable.
-     *
-     * @param  string    $message  - An optional error message for this throwable.
-     * @param  int       $code     - An optional error code for this throwable.
-     * @param ?Throwable $previous - An optional throwable, which were thrown prior to this throwable.
-     *
-     * @constructor
-     */
-    public function __construct(string $message = '', int $code = self::CODE_DEFAULT, ?Throwable $previous = null)
-    {
-        // Call the parent constructor.
-        parent::__construct($message, $code, $previous);
-    }
-
-    /**
-     * Returns a string representation of this throwable.
-     *
-     * @return string - A string representation of this throwable.
-     */
-    public function __toString(): string
-    {
-        return static::class . ' (' . $this->code . '): ' . $this->message;
-    }
-}
+class Exception extends RuntimeException {}
